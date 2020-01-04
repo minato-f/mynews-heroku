1f65cf9a578c256f2480bfd9b0974d4e01867901
minato-f <minato.furuno.ces66@gmail.com>
2020-01-04 07:03:38 +0000

    PHP/Laravel 18 課題提出

--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'NewsController@index');
+
+Route::get('/profile', 'ProfileController@index');
